cbb05395572fe996e9085431f2756be608925bac
jedait <jedait@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-15 08:13:46 +0000

    Add 1.1 version.
    git-svn-id: https://plugins.svn.wordpress.org/definition-list/tags/1.1@263688 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ panel.php
@@ -0,0 +1,33 @@
+<?php
+define('WP_USE_THEMES', false);
+require ('../../../wp-blog-header.php');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Definition List<title>
+<script type="text/javascript"
+    src="../../../wp-includes/js/tinymce/tiny_mce_popup.js"></script>
+
+<script language="javascript">
+
+function dd_insert() {
+    var ed = tinyMCEPopup.editor;
+    var term = document.getElementById('dd_term').value;
+    var desc = document.getElementById('dd_desc').value;
+    ed.selection.setContent('<dl><dt>' + term + '</dt><dd>' + desc + '</dd></dl>');
+    tinyMCEPopup.close();
+}
+
+</script>
+</head>
+
+<body>
+<form>
+    <p>Term<br/><input name="Term" id="dd_term" type="text" size="91"/></p>
+    <p>Definition<br/><textarea name="Definition" id="dd_desc" cols="90" rows="20"></textarea></p>
+    <p><input type="button" value="insert" onclick="dd_insert();" style="width: 120" /></p>
+</form>
+</body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Definition List ===
+Contributors: jedait
+Donate link: http://cirux.ru
+Tags: definition-list, dd, dt, dl, definition, list
+Requires at least: 2.0.2
+Tested up to: 3.0
+Stable tag: 1.1
+
+Definition List plugin add button to rich editor. Using it you can easily insert definition list elements to your articles.
+
+== Description ==
+
+Definition List plugin add button to rich editor. Using it you can easily insert definition list elements to your articles.
+
+== Installation ==
+
+1. Upload `definition-list` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+== Changelog ==
+
+= 1.1 =
+* Rename fields.
+
+= 1.0 =
+* First stable version.
+
